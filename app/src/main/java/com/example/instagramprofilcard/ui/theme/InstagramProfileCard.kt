38d7f36ee738445a7960e2c5f41f2f411ff1324a
Andrewka <Uzzbagoyzza@yandex.ru>
2025-07-16 23:58:51 +0300

    9 урок. Разобрали порядок modifire

--- app/src/main/java/com/example/instagramprofilcard/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/instagramprofilcard/ui/theme/InstagramProfileCard.kt
@@ -2,6 +2,7 @@ package com.example.instagramprofilcard.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -17,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -50,7 +54,10 @@ fun InstagramProfileCard() {
         ) {
             Image(
                 modifier = Modifier
-                    .size(50.dp),
+                    .clip(shape = CircleShape)
+                    .background(Color.White)
+                    .padding(8.dp)
+                    .size(60.dp),
                 painter = painterResource(R.drawable.ic_instagram),
                 contentDescription = "profile icon",
                 contentScale = ContentScale.Crop
