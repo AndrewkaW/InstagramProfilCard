269a51555bda10af6eb4bba1abc598e1d2ccdecc
Andrewka <Uzzbagoyzza@yandex.ru>
2025-09-05 15:24:09 +0300

    Практика с стейтом списка.

--- app/src/main/java/com/example/instagramprofilcard/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofilcard/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.ViewModelProvider
 import com.example.instagramprofilcard.ui.theme.InstagramProfileCard
+import com.example.instagramprofilcard.ui.theme.InstagramProfileCardTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -18,17 +23,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
-
-            val model = viewModel.models.observeAsState(listOf())
-            LazyColumn {
-                items(model.value) { model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowButtonClickListener = { viewModel.changeFollowingStatus(model) }
-                    )
+            InstagramProfileCardTheme {
+                val model = viewModel.models.observeAsState(listOf())
+                val lazyListState = rememberLazyListState()
+                val scope = rememberCoroutineScope()
+                LazyColumn(
+                    state = lazyListState
+                ) {
+                    items(model.value) { model ->
+                        InstagramProfileCard(
+                            model = model,
+                            onFollowButtonClickListener = { viewModel.changeFollowingStatus(model) }
+                        )
+                    }
                 }
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            lazyListState.scrollToItem(0)
+                        }
+                    }
+                ) { }
             }
         }
     }
