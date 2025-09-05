f90465506914a6f104114ef300cac31c3b80ba88
Andrewka <Uzzbagoyzza@yandex.ru>
2025-09-05 16:49:30 +0300

    Swipe to dismiss

--- app/src/main/java/com/example/instagramprofilcard/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofilcard/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.example.instagramprofilcard
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.instagramprofilcard.ui.theme.InstagramProfileCard
 import com.example.instagramprofilcard.ui.theme.InstagramProfileCardTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
@@ -28,25 +36,59 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstagramProfileCardTheme {
                 val model = viewModel.models.observeAsState(listOf())
-                val lazyListState = rememberLazyListState()
-                val scope = rememberCoroutineScope()
-                LazyColumn(
-                    state = lazyListState
-                ) {
-                    items(model.value) { model ->
-                        InstagramProfileCard(
-                            model = model,
-                            onFollowButtonClickListener = { viewModel.changeFollowingStatus(model) }
+                LazyColumn {
+                    items(items = model.value, key = { it.id }) { model ->
+
+                        val dismissBoxState = rememberSwipeToDismissBoxState(
+                            confirmValueChange = { value ->
+                                val isDismissed = value in setOf(
+                                    SwipeToDismissBoxValue.StartToEnd,
+                                    SwipeToDismissBoxValue.EndToStart
+                                )
+                                if (isDismissed) {
+                                    viewModel.delete(model)
+                                }
+                                return@rememberSwipeToDismissBoxState isDismissed
+                            }
+
                         )
+
+
+                        SwipeToDismissBox(
+                            state = dismissBoxState,
+                            enableDismissFromStartToEnd = false,
+                            enableDismissFromEndToStart = true,
+                            backgroundContent = {
+                                Box(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxSize()
+                                        .background(
+                                            color = Color.Red.copy(alpha = 0.5f)
+                                        ),
+                                    contentAlignment = Alignment.CenterEnd
+                                ) {
+                                    Text(
+                                        modifier = Modifier.padding(16.dp),
+                                        text = "Delete Item",
+                                        color = Color.White,
+                                        fontSize = 24.sp
+                                    )
+                                }
+                            }
+                        ) {
+                            InstagramProfileCard(
+                                model = model,
+                                onFollowButtonClickListener = {
+                                    viewModel.changeFollowingStatus(
+                                        model
+                                    )
+                                }
+                            )
+                        }
+
                     }
                 }
-                FloatingActionButton(
-                    onClick = {
-                        scope.launch {
-                            lazyListState.scrollToItem(0)
-                        }
-                    }
-                ) { }
             }
         }
     }
